e7691b086d54bf3e55a64c65fc1a2caa9a08a8e1
Kevin Von Erich Peredo <kveperedo@gmail.com>
2021-04-04 23:55:43 +0800

    add experimental color about me

--- components/aboutme.tsx
+++ components/aboutme.tsx
@@ -2,25 +2,41 @@ import aboutMeStyles from './aboutme.module.css';
 
 const AboutMe = () => {
 	return (
-		<div className={aboutMeStyles.container}>
-			<h3 className={aboutMeStyles.header}>About Me</h3>
-			<p>Hello! My name is Kevin. I am 23 years old.</p>
-			<p>I am an electronics engineering graduate from Saint Louis University in Baguio City.</p>
-			<p>Passed the electronics engineering and electronics technician board exams in 2019.</p>
-			<p>
-				I am a self-taught Front-End Developer with knowledge in HTML, CSS, JavaScript and currently learning TypeScript
-				and React frameworks.
-			</p>
-			<p>Currently working at SQREEM Technologies as a software engineer since 2020. </p>
-			<p>
-				My hobbies are: love for computers and mechanical keyboards, playing badminton, watching Netflix and YouTube,
-				playing various video games, learning software technologies and admiring aesthetic UI designs.
-			</p>
-			<p>
-				Music genres that I enjoy are RnB, Jazz Lo-FI, Funk and Electronic Pop. You can follow my Spotify playlist here:
-				sptfy.com/kebon
-			</p>
-			<p>My ultimate goal is to become a Full Stack Engineer.</p>
+		<div className={aboutMeStyles.main}>
+			<div className={aboutMeStyles.container}>
+				<h3 className={aboutMeStyles.header}>ABOUT ME</h3>
+				<p>
+					Hello! My name is <span className={aboutMeStyles.blue}>Kevin</span>. I am 23 years old.
+				</p>
+				<p>
+					I am an <span className={aboutMeStyles.orange}>electronics engineering</span> graduate from Saint Louis
+					University in Baguio City.
+				</p>
+				<p>
+					Passed the electronics engineering and electronics technician{' '}
+					<span className={aboutMeStyles.green}>board exams</span> in 2019.
+				</p>
+				<p>
+					I am a self-taught <span className={aboutMeStyles.yellow}>Front-End Developer</span> with knowledge in HTML,
+					CSS, JavaScript and currently learning TypeScript and React frameworks.
+				</p>
+				<p>
+					Currently working at <span className={aboutMeStyles.blue}>SQREEM Technologies</span> as a software engineer
+					since 2020.{' '}
+				</p>
+				<p>
+					My <span className={aboutMeStyles.orange}>hobbies</span> are: love for computers and mechanical keyboards,
+					playing badminton, watching Netflix and YouTube, playing various video games, learning software technologies
+					and admiring aesthetic UI designs.
+				</p>
+				<p>
+					<span className={aboutMeStyles.yellow}>Music genres</span> that I enjoy are RnB, Jazz Lo-FI, Funk and
+					Electronic Pop. You can follow my Spotify playlist here: sptfy.com/kebon
+				</p>
+				<p>
+					My ultimate goal is to become a <span className={aboutMeStyles.green}>Full Stack Engineer</span>.
+				</p>
+			</div>
 		</div>
 	);
 };
--- components/home.tsx
+++ components/home.tsx
@@ -38,7 +38,7 @@ const Home = () => {
 				</section>
 
 				<div className={homeStyles.card}>
-					<button className={homeStyles.resume} onClick={() => window.open('/Resume.pdf')}>
+					<button className={homeStyles.resume} onClick={() => window.open('/resumenew.pdf')}>
 						Get Resume
 					</button>
 					<div className={homeStyles.contact}>
